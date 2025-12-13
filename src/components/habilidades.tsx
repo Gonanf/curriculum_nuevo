c8fc4a632bb0ca96c662591b0ec540bce9757514
Gonanf <gabysolotorevsky100@gmail.com>
2025-12-13 20:20:09 -0300

    feature(reworking everything)

--- src/components/habilidades.tsx
+++ src/components/habilidades.tsx
@@ -1,17 +1,48 @@
 import type { ReactNode } from "react";
 import ParallaxSection from "./parallax";
 import { motion } from "motion/react";
+import { useState } from "react";
 
-function Item({ children }: { children: ReactNode }) {
-  return (<motion.div className="flex gap-4 items-center rounded-full" initial={{ scale: 1, color: "var(--muted)", textShadow: "0 0 0" }} whileHover={"hover"} variants={{ hover: { scale: 1.1, color: "white", textShadow: "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #0073e6", backgroundColor: "rgba(100,100,100,1)", padding: "10%" } }}>
-    <motion.div className="w-5 h-5 bg-linear-120 from-cyan-500 to-amber-950 rounded-full" variants={{ hover: { filter: "blur(5px)" } }}></motion.div>
-    <motion.p className={"text-xl"} >{children}</motion.p>
-  </motion.div>)
+function Item({ children, onHover, index }: { children: ReactNode; onHover: (index: number) => void, index: number }) {
+  return (
+    <motion.div 
+      className="flex gap-4 items-center rounded-full cursor-pointer" 
+      initial={{ scale: 1, color: "var(--muted)", textShadow: "0 0 0" }} 
+      whileHover={"hover"} 
+      variants={{ 
+        hover: { 
+          scale: 1.1, 
+          color: "white", 
+          textShadow: "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #0073e6", 
+          backgroundColor: "rgba(100,100,100,1)", 
+          padding: "10%" 
+        } 
+      }}
+      onHoverStart={() => {
+        onHover(index);
+      }}
+      onHoverEnd={() => onHover(0)}
+    >
+      <motion.div 
+        className="w-5 h-5 bg-linear-120 from-cyan-500 to-amber-950 rounded-full" 
+        variants={{ hover: { filter: "blur(5px)" } }}
+      ></motion.div>
+      <motion.p className="text-xl">{children}</motion.p>
+    </motion.div>
+  )
 }
 
 function Habilidades() {
   const seccion1 = ["Rust", "Javascript", "Typescript", "Python", "C++", "HTML", "CSS", "Vue", "Nuxt"]
   const seccion2 = ["ShadCN UI", "TailwindCSS", "Git", "Github", "Linux", "Arch", "Cloudflare", "API Rest", "POO"]
+  const images = ["/rust2.png", "/ts.png", "/js.png", "/python.png", "/cpp.png", "/html.png", "/css.png", "/vue.png", "/nuxt.png","/shadcn.png"]
+  
+  const [currentImage, setCurrentImage] = useState("/rust2.png")
+
+  const handleImageHover = (index: number) => {
+    //TODO: Maybe later
+    //setCurrentImage(images[index])
+  }
 
   return (
     <ParallaxSection className={"bg-amber-950/50 bg-[url('/kira2.png')] bg-cover bg-center"} contentClass="sticky inset-0 min-h-screen flex flex-col backdrop-blur-sm">
@@ -20,13 +51,16 @@ function Habilidades() {
 
         <div className='flex grow justify-center gap-1.5 border-t-4 border-fuchsia-950 p-4' >
           <div className="items-end justify-between flex flex-col grow">
-            {seccion1.map((val) => <Item>{val}</Item>)}
+            {seccion1.map((val, index) => <Item key={val} onHover={handleImageHover} index={index}>{val}</Item>)}
           </div>
 
-          <motion.img src="/rust2.png" className="h-full grow self-center drop-shadow-2xl drop-shadow-amber-300/50 " variants={{ hover: { rotate: 5 } }} drag dragConstraints={{ top: -10, bottom: 10, left: -10, right: 10 }} />
+          <motion.img 
+            src={currentImage} 
+            className="h-[90%] w-[90%] self-center drop-shadow-2xl drop-shadow-amber-300/50 " 
+          />
 
           <div className="grow flex flex-col justify-between">
-            {seccion2.map((val) => <Item>{val}</Item>)}
+            {seccion2.map((val, index) => <Item key={val} onHover={handleImageHover} index={index + seccion1.length}>{val}</Item>)}
           </div>
         </div>
 
