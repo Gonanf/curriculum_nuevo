be3d842781df931ea28b4faccf1dfb025d59439b
Gonanf <gabysolotorevsky100@gmail.com>
2025-12-15 19:28:04 -0300

    feature(finished?)

--- src/components/contacto.tsx
+++ src/components/contacto.tsx
@@ -20,7 +20,7 @@ function ProjectCard({ project }: { project: Contact }) {
     hover: { color: statusColors(1) }
   }
   return (
-    <motion.div
+    <motion.a
       initial={{ opacity: 0, y: 50, scale: 0.9 }}
       whileInView={{ opacity: 1, y: 0, scale: 1 }}
       transition={{
@@ -37,11 +37,12 @@ function ProjectCard({ project }: { project: Contact }) {
       }}
       className="relative"
       style={{ filter: `drop-shadow(0 3px 3px ${statusColors(1)})` }}
+      href={project.url}
     >
       <div className="bg-black/95 rounded-2xl sm:rounded-3xl p-4 sm:p-6 h-full flex flex-col" style={{ border: `2px solid ${statusColors(0.4)}` }}>
-        <div className="mb-4 flex flex-col justify-between items-center">
-          <div className="grow *:w-full *:h-full">
-            {project.logo}
+        <div className="mb-4 flex flex-col justify-between items-center flex-1">
+          <div className="grow w-full h-full *:w-full *:h-full flex items-center justify-center">
+              {project.logo}
           </div>
           <motion.h3 className="text-lg sm:text-xl text-center font-bold mb-1" initial={{ color: 'oklch(98.2% 0.018 155.826)' }} transition={{ duration: 0.2, ease: easeInOut }} variants={hover_variant}>
             {project.titulo}
@@ -49,7 +50,8 @@ function ProjectCard({ project }: { project: Contact }) {
         </div>
 
       </div>
-    </motion.div>
+        <motion.div className="absolute inset-0 z-20 w-full h-full rounded-3xl pointer-events-none" style={{ backgroundImage: `linear-gradient(90deg,${statusColors(0.1)} 0%, ${statusColors(0.4)} 100%)` }} initial={{ opacity: 0 }} variants={{ hover: { opacity: 100 } }}></motion.div>
+    </motion.a>
   );
 }
 
@@ -58,23 +60,23 @@ function Contacto() {
   const contactos: Array<Contact> = [
     {
       logo: <svg role="img" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>,
-      titulo: "Github",
+      titulo: "Gonanf",
       color: "44.6% 0.043 257.281",
       url: "https://github.com/Gonanf"
     },
 
     {
       logo: <LucideMail></LucideMail>,
-      titulo: "Github",
-      color: "44.6% 0.043 257.281",
-      url: "https://github.com/Gonanf"
+      titulo: "solotorevskygabrieltrabajo@gmail.com",
+      color: "74% 0.238 322.16",
+      url: "mailto:solotorevskygabrieltrabajo@gmail.com"
     },
 
     {
       logo: <img src='/InBug-White.png' />,
-      titulo: "Github",
-      color: "44.6% 0.043 257.281",
-      url: "https://github.com/Gonanf"
+      titulo: "Gabriel Solotorevsky",
+      color: "62.3% 0.214 259.815",
+      url: "https://www.linkedin.com/in/gabriel-solotorevsky-35524431b"
     },
   ]
 
